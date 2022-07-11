cf554017bcd5337c3e380df210072c49f2fe887f
ThanasisKl <tklettas@gmail.com>
2022-07-11 13:32:31 +0300

    first commit

---
+++ MyNotesApp/Controllers/NoteController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyNotesApp.Data;
+using MyNotesApp.Models;
+
+namespace MyNotesApp.Controllers
+{
+    public class NoteController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public NoteController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Note> objCategoryList = _db.Notes;
+            return View(objCategoryList);
+        }
+    }
+}
---
+++ MyNotesApp/Models/Note.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyNotesApp.Models
+{
+    public class Note
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        [Required]
+        public string Type { get; set; }
+
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
