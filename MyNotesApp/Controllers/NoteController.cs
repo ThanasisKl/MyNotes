3444f8314c2ee71e757f6502f106b26d64fda96e
ThanasisKl <tklettas@gmail.com>
2022-07-12 00:43:33 +0300

    add delete functionality and contacts page

--- MyNotesApp/Controllers/NoteController.cs
+++ MyNotesApp/Controllers/NoteController.cs
@@ -16,5 +16,66 @@ namespace MyNotesApp.Controllers
             IEnumerable<Note> objCategoryList = _db.Notes;
             return View(objCategoryList);
         }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Note obj)
+        {
+            /*if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }*/
+            if (ModelState.IsValid)
+            {
+                _db.Notes.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var noteFromDb = _db.Notes.Find(id);
+            //var categoryFromDbFirst = _db.Categories.FirstOrDefault(u=>u.Id==id);
+            //var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+
+            if (noteFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(noteFromDb);
+        }
+
+        //POST
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _db.Notes.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Notes.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
+
+        }
     }
 }
+
